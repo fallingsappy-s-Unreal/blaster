4dab30e76260f32334f61ca81994084f385f53cc
Vladimir Novikov <novikovvlal@gmail.com>
2022-05-27 09:10:21 +0300

    Aim Walking

--- Source/Blaster/Characters/MainCharacter/BlasterCharacter.cpp
+++ Source/Blaster/Characters/MainCharacter/BlasterCharacter.cpp
@@ -6,6 +6,7 @@
 #include "Blaster/Components/MainCharacter/CombatComponent.h"
 #include "Blaster/Weapons/Weapon.h"
 #include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
 #include "Components/WidgetComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
@@ -34,6 +35,9 @@ ABlasterCharacter::ABlasterCharacter()
 	Combat->SetIsReplicated(true);
 
 	GetCharacterMovement()->NavAgentProps.bCanCrouch = true;
+
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	GetMesh()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
 }
 
 void ABlasterCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const

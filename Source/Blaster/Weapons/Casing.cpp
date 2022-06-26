9e223063b424ae5f0a8744196199335fe281eb78
Vladimir Novikov <novikovvlal@gmail.com>
2022-06-26 10:07:14 +0300

    Shell physics and sound

--- Source/Blaster/Weapons/Casing.cpp
+++ Source/Blaster/Weapons/Casing.cpp
@@ -1,15 +1,38 @@
 #include "Casing.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+
 ACasing::ACasing()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
 	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
 	SetRootComponent(CasingMesh);
+	CasingMesh->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetEnableGravity(true);
+	CasingMesh->SetNotifyRigidBodyCollision(true);
+	ShellEjectionImpulse = 10.f;
 }
 
 void ACasing::BeginPlay()
 {
 	Super::BeginPlay();
+
+	CasingMesh->OnComponentHit.AddDynamic(this, &ACasing::OnHit);
+	CasingMesh->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
+
+	AActor::SetLifeSpan(3.f);
+}
+
+void ACasing::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (ShellSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+	}
 	
+	CasingMesh->SetNotifyRigidBodyCollision(false);
 }

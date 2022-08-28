081712345068612d2e941b19302ab6aef709ac29
Vladimir Novikov <novikovvlal@gmail.com>
2022-08-28 15:43:17 +0300

    Grenade Launcher

--- Source/Blaster/Components/MainCharacter/CombatComponent.cpp
+++ Source/Blaster/Components/MainCharacter/CombatComponent.cpp
@@ -509,6 +509,7 @@ void UCombatComponent::InitializeCarriedAmmo()
 	CarriedAmmoMap.Emplace(EWeaponType::EWT_SubmachineGun, StartingSMGAmmo);
 	CarriedAmmoMap.Emplace(EWeaponType::EWT_Shotgun, StartingShotgunAmmo);
 	CarriedAmmoMap.Emplace(EWeaponType::EWT_SniperRifle, StartingSniperRifleAmmo);
+	CarriedAmmoMap.Emplace(EWeaponType::EWT_GrenadeLauncher, StartingGrenadeLauncherAmmo);
 }
 
 void UCombatComponent::PlayEquipSound()
--- Source/Blaster/Components/MainCharacter/CombatComponent.h
+++ Source/Blaster/Components/MainCharacter/CombatComponent.h
@@ -152,6 +152,9 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	int32 StartingSniperRifleAmmo = 0;
+
+	UPROPERTY(EditAnywhere)
+	int32 StartingGrenadeLauncherAmmo = 0;
 	
 	void InitializeCarriedAmmo();
 

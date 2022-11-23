ba993d02db9a56c49fede461bf989b53386c8ebc
Vladimir Novikov <novikovvlal@gmail.com>
2022-11-23 21:02:56 +0300

    Return to Main Menu

--- Source/Blaster/Blaster.Build.cs
+++ Source/Blaster/Blaster.Build.cs
@@ -8,7 +8,7 @@ public class Blaster : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystemSteam", "OnlineSubsystem", "Niagara" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystemSteam", "OnlineSubsystem", "Niagara", "MultiplayerSessions" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
